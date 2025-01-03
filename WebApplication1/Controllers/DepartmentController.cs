506abd8504c9c7e4f1df28b56d7dd782ed0bb4bf
Satish Timalsina <timalsinasatish430@gmail.com>
2025-01-03 06:57:25 +0545

    Second commit

--- WebApplication1/Controllers/DepartmentController.cs
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -1,17 +1,90 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class DepartmentController : Controller
     {
+        private readonly IMemoryCache _cache;
+
+        public DepartmentController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
         public IActionResult Index()
         {
-            Something service = new Something();
-            var deplist = service.GetDependent();
-            return View(deplist);
-           
+            const string cacheKey = "studentList";
+            if (!_cache.TryGetValue(cacheKey, out List<Student> studentList))
+            {
+                // Assuming "Something" is a service; replace with an actual implementation
+                Something service = new Something();
+                studentList = service.GetDependent();
+
+                // Cache the data
+                _cache.Set(cacheKey, studentList, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30) // Set an appropriate cache expiration
+                });
+            }
+
+            return View(studentList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new Student();
+            return View(model);
+        }
+        public IActionResult Edit()
+        {
+            var model = new Student();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            const string cacheKey = "studentList";
+            if (!_cache.TryGetValue(cacheKey, out List<Student> studentList))
+            {
+                studentList = new List<Student>();
+            }
+
+            studentList.Add(model);
+
+            // Update the cache
+            _cache.Set(cacheKey, studentList);
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            object obj = _cache.Get("studentList");
+            var dList=(List<Student>)obj;
+            var model=new Student();
+            var department = dList.FirstOrDefault(e => e.Roll == id);
+            if (department is not null)
+                model = department;
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(Student model) {
+            object obj = _cache.Get("studentList");
+            var dList = (List<Student>)obj;
+            foreach(var d in dList)
+            {
+                if (d.Roll == model.Roll)
+                {
+                    d.Name = model.Name;
+                    break;
+                }
+            }
+            _cache.Set("studentList", dList);
+            return RedirectToAction("Index");
         }
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d8fa2a849d372a18c79b7d92a8d46c0059e2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972e8c17db2863b819d4299acf9c8e0ba4ed35a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
